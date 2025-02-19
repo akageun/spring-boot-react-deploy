fb97294bcdd8cebbc971cc16face25cbc71f13df
akageun <akageun@gmail.com>
2025-02-19 22:11:00 +0900

    [#noissue] 추가

--- src/main/webapp/src/pages/Page1.jsx
+++ src/main/webapp/src/pages/Page1.jsx
@@ -1,21 +1,11 @@
-import {ServerMap} from "@pinpoint-fe/server-map";
 import {useState} from "react";
 
 const Page1 = () => {
-    const [data] = useState({
-        nodes: [{id: '1', label: 'test'}, {id: '2', label: 'test2'}],
-        edges: [{id: 'e1', source: '1', target: '2'}]
-    });
-    return (
-        <div className="App">
-
-            <ServerMap
-                data={data}
-                baseNodeId={'MY-APP'}
-            />
-
-        </div>
-    );
+  return (
+    <div className="App">
+      Page 1
+    </div>
+  );
 }
 
 export default Page1;
